53744fe82c0aedfc4491af0a76e64dbca2392c86
Jeongmin Kim <jmkim@iconloop.com>
2021-06-02 09:05:35 +0900

    Initial Commit

---
+++ URLSessionFetchingData/URLSessionFetchingDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionFetchingDataApp.swift
+//  URLSessionFetchingData
+//
+//  Created by 김정민 on 2021/06/02.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionFetchingDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
